ffd43df99f4e4ec91f3cd1925abb05c25b418f63
Rurien <a01092603909@gmail.com>
2025-12-26 12:50:31 +0900

    Add layout header

---
+++ src/layout/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.header`
+  background-color: #4285f4;
+  color: #fff;
+  padding: 20px;
+`;
+
+const Logo = styled.img`
+  width: 150px;
+`;
+
+const Nav = styled.nav`
+  margin-top: 10px;
+`;
+
+const NavLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 20px;
+  font-size: 10px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Header = () => {
+  return (
+    <Container>
+      <Logo src='https://avatars.githubusercontent.com/u/89754008' />
+      <Nav>
+        <NavLink href='#'>Home</NavLink>
+        <NavLink href='#'>About</NavLink>
+        <NavLink href='https://github.com/Rurien1124'>GitHub</NavLink>
+      </Nav>
+    </Container>
+  );
+};
