62859451e6db6e5bd095fe2626c4d58ab3ac65b4
Rurien <a01092603909@gmail.com>
2025-12-29 14:56:40 +0900

    Separate examples paths to each example

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Layout } from './layouts/main/Layout';
 import { MainPage } from './pages/main';
-import { ExamplesPage } from './pages/examples';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { ErrorPage } from './pages/error';
+import {
+  ExamplesContextPage,
+  ExamplesNavigationPage,
+  ExamplesPropsPage,
+  ExamplesStatePage,
+} from './pages/examples';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='/examples' element={<ExamplesPage />} />
+        <Route path='/examples'>
+          <Route index element={<Navigate to='props' replace />} />
+          <Route path='props' element={<ExamplesPropsPage />} />
+          <Route path='state' element={<ExamplesStatePage />} />
+          <Route path='context' element={<ExamplesContextPage />} />
+          <Route path='navigation' element={<ExamplesNavigationPage />} />
+        </Route>
         <Route
           path={'*'}
           element={<ErrorPage text={'NOT FOUND'} code={404} />}
