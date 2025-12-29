31fedbbeee98ac10c073bb58f50689dc90476fd6
Rurien <a01092603909@gmail.com>
2025-12-29 14:04:05 +0900

    Change app components to Route component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { Layout } from './layouts/main/Layout';
 import { MainPage } from './pages/main';
 import { Example } from './pages/example';
+import { Route, Routes } from 'react-router-dom';
+import { ErrorPage } from './pages/error';
 
 function App() {
   return (
     <Layout>
-      <Example />
-      <MainPage />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/example' element={<Example />} />
+        <Route
+          path={'*'}
+          element={<ErrorPage text={'NOT FOUND'} code={404} />}
+        />
+      </Routes>
     </Layout>
   );
 }
