28ad1423abf4d2261dabf04fa714ea52abec548c
Rurien <a01092603909@gmail.com>
2025-12-29 14:59:38 +0900

    Remove unused imports

--- src/layouts/main/Header.tsx
+++ src/layouts/main/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Container = styled.header`
   background-color: #4285f4;
